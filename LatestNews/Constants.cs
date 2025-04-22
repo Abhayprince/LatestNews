8cd74605548f3fdcd7bd4a79011fee1073a0a1bd
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2025-04-23 00:03:28 +0530

    Add note

--- LatestNews/Constants.cs
+++ LatestNews/Constants.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace LatestNews;
 public static class Constants
 {
+    // Note: I know this is not a good idea to have the Api Key in here in Client MAUI 
+    // for the demo purpose, I am keeping it simple and having the api key here (Which is NOT SECURE)
+    // Should not be like this for Production use
+
     // GRAB YOUR API KEY FROM - https://newsdata.io/
     public const string ApiKey = "YOUR_API_KEY";
 }
